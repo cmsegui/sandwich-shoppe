810018aee3e9ee8a521b80d710d5d8eaec35e4ba
cmsegui <cmsegui@yahoo.com>
2017-12-08 00:06:36 -0500

    made basic layout for sandwich builder component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
-
+import SandwichBuilder from './containers/SandwichBuilder/SandwichBuilder';
 
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <p>TESTING 123</p>
+          <SandwichBuilder/>
         </Layout>
       </div>
     );
