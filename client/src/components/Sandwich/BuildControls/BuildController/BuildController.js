24dcff0a8e93afdb0942bbe18358f389cd9102a6
cmsegui <cmsegui@yahoo.com>
2017-12-09 18:15:44 -0500

    added logic for removing ingredient and found bug on tomato so corrected that

--- client/src/components/Sandwich/BuildControls/BuildController/BuildController.js
+++ client/src/components/Sandwich/BuildControls/BuildController/BuildController.js
@@ -5,7 +5,7 @@ import classes from './BuildController.css';
 const buildController = (props) => (
     <div className={classes.BuildController}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>Less</button>
+        <button className={classes.Less} onClick={props.removed}>Less</button>
         <button className={classes.More} onClick={props.added}>More</button>
     </div>
 );
--- client/src/containers/SandwichBuilder/SandwichBuilder.js
+++ client/src/containers/SandwichBuilder/SandwichBuilder.js
@@ -15,6 +15,7 @@ class SandwichBuilder extends Component {
     state = {
         ingredients: {
             salad: 0, 
+            tomato: 0,
             bacon: 0, 
             cheese: 0, 
             meat: 0
@@ -35,15 +36,26 @@ class SandwichBuilder extends Component {
         this.setState( { totalPrice: newPrice, ingredients: updatedIngredients } );
     }
 
-    removeIngredientHandler = {
-
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState( { totalPrice: newPrice, ingredients: updatedIngredients } );
     }
 
     render() {
         return(
             <Aux>
                 <Sandwich ingredients={this.state.ingredients} />
-                <BuildControls ingredientAdded={this.addIngredientHandler} />
+                <BuildControls ingredientAdded={this.addIngredientHandler}
+                               ingredientRemoved={this.removeIngredientHandler}
+                />
             </Aux>
         );
     }
