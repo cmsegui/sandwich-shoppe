e5066edea7d89cf3cfc22cec34b3c75b3456fc79
cmsegui <cmsegui@yahoo.com>
2018-01-20 17:00:36 -0500

    added dynamic navigation links to the toolbar and styled them for desktop, mobile will come later

---
+++ client/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './NavigationItems.css';
+import NavItem from './NavItem/NavItem'
+
+const navigationItems = (props) => (
+    <ul className={classes.NavigationItems}>
+        <NavItem link="/" active>Sandwich Builder</NavItem>
+        <NavItem link="/">Checkout</NavItem>
+    </ul>
+);
+
+
+export default navigationItems;
