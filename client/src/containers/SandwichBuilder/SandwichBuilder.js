0da2870649632f2015d9a5fdaeef39badc820157
cmsegui <cmsegui@yahoo.com>
2018-01-22 17:07:20 -0500

    added logic to make spinner pop up in the modal

--- client/src/containers/SandwichBuilder/SandwichBuilder.js
+++ client/src/containers/SandwichBuilder/SandwichBuilder.js
@@ -4,6 +4,7 @@ import Sandwich from '../../components/Sandwich/Sandwich';
 import BuildControls from '../../components/Sandwich/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Sandwich/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-orders';
 
 
@@ -26,7 +27,8 @@ class SandwichBuilder extends Component {
         },
         totalPrice: 4,
         purchasable: false, 
-        purchasing: false 
+        purchasing: false,
+        loading: false 
     }
 
     updatePurchaseState (ingredients) {
@@ -81,6 +83,7 @@ class SandwichBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert("Let's Continue!");
+        this.setState({ loading: true });
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -96,8 +99,12 @@ class SandwichBuilder extends Component {
             deliveryMethod: 'ASAP'
         }
         axios.post('/orders.json', order)
-            .then(response => console.log(response))
-            .catch(error => console.log(error));
+            .then(response => {
+                this.setState({ loading: false, purchasing: false });
+            })
+            .catch(error => {
+                this.setState({ loading: false, purchasing: false });
+            });
         
     }
 
@@ -108,14 +115,19 @@ class SandwichBuilder extends Component {
         for (let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
+        let orderSummary = <OrderSummary 
+            ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}/>
+        if (this.state.loading) {
+            orderSummary = <Spinner />;
+        }
+
         return(
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
-                        ingredients={this.state.ingredients}
-                        price={this.state.totalPrice}
-                        purchaseCancelled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandler}/>
+                    {orderSummary}
                 </Modal>
                 <Sandwich ingredients={this.state.ingredients} />
                 <BuildControls ingredientAdded={this.addIngredientHandler}
