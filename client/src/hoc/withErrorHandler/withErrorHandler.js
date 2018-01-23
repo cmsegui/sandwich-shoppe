a28f1c43f6c87efbbbf0425c0199b9b2be633b28
cmsegui <cmsegui@yahoo.com>
2018-01-22 19:32:03 -0500

    removed line that was not needed

--- client/src/hoc/withErrorHandler/withErrorHandler.js
+++ client/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
-import axios from '../../axios-orders';
 
 
 const withErrorHandler = (WrappedComponent, axios) => {
