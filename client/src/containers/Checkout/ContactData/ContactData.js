d5a7f9a58c1ad3f6c5a59bb0cea2a6046c12d916
cmsegui <cmsegui@yahoo.com>
2018-01-31 00:33:57 -0500

    added logic to submit orders and save them in database and also pass that data to other pages

--- client/src/containers/Checkout/ContactData/ContactData.js
+++ client/src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
-import classes from './ContactData.css'
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import classes from './ContactData.css';
+import axios from '../../../axios-orders';
+
 
 class ContactData extends Component {
     state = {
@@ -10,21 +13,57 @@ class ContactData extends Component {
             street: '', 
             city: '', 
             zipCode: ''
+        },
+        loading: false
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        console.log(this.state.ingredients);
+
+        this.setState({ loading: true });
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Mia Bones',
+                address: {
+                    street: '123 Rockingway Street',
+                    zipCode: '30303',
+                    state: 'Georgia'
+                },
+                email: 'mia@email.com',
+            },
+            deliveryMethod: 'ASAP'
         }
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({ loading: false });
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                this.setState({ loading: false });
+            });
     }
 
     render() {
+        let form = (
+            <form>
+                <input className={classes.Input} type="text" name="name" placeholder="Name" />
+                <input className={classes.Input} type="email" name="email" placeholder="Email" />
+                <input className={classes.Input} type="text" name="street" placeholder="Street" />
+                <input className={classes.Input} type="text" name="city" placeholder="City" />
+                <input className={classes.Input} type="text" name="zipCode" placeholder="Zip Code" />
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner />;
+        }
         return(
             <div className={classes.ContactData}>
                 <h4>Enter Your Contact Information</h4>
-                <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Name" />
-                    <input className={classes.Input} type="email" name="email" placeholder="Email" />
-                    <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                    <input className={classes.Input} type="text" name="city" placeholder="City" />
-                    <input className={classes.Input} type="text" name="zipCode" placeholder="Zip Code" />
-                    <Button btnType="Success">ORDER</Button>
-                </form>
+                {form}
             </div>
         );
     }
--- client/src/containers/SandwichBuilder/SandwichBuilder.js
+++ client/src/containers/SandwichBuilder/SandwichBuilder.js
@@ -90,32 +90,12 @@ class SandwichBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert("Let's Continue!");
-        // this.setState({ loading: true });
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Mia Bones',
-        //         address: {
-        //             street: '123 Rockingway Street',
-        //             zipCode: '30303',
-        //             state: 'Georgia'
-        //         },
-        //         email: 'mia@email.com',
-        //     },
-        //     deliveryMethod: 'ASAP'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     })
-        //     .catch(error => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     });
+        
         const queryParams = [];
         for(let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout', 
